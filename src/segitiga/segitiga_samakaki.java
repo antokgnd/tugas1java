7ed44d4c047177501728b52ca03cc60436e8f028
antokgnd <pemudat45@gmail.com>
2016-11-04 08:07:18 +0700

    Update segitiga_samakaki.java

--- src/segitiga/segitiga_samakaki.java
+++ src/segitiga/segitiga_samakaki.java
@@ -3,36 +3,36 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package segitiga;
-import java.util.Scanner;
+package segitiga; 			//menjelaskan dari induk folder
+import java.util.Scanner;    		//mempunyai fungsi untuk mengaktifkan fungsi scanner
 
 /**
  *
  * @author Antok
  */
-public class segitiga_samakaki {
+public class segitiga_samakaki {			/**merupakan sebuah penampung class */
     public static void main(String [] args){
         
         Scanner scan = new Scanner(System.in);
-        int x;
+        int x; 						/**merupakan nilai  inputan dari interger */
         
-        boolean menu =true;
+        boolean menu =true;		/**merupakan salahsatu tipe data yang yang berguna untuk memunculkan nilai pembanding */
         while(menu)
         {
-        	System.out.print("Masukkan nilai x =");
-        	x = scan.nextInt();
+        	System.out.print("Masukkan nilai x =");		
+        	x = scan.nextInt();			//** berguna untuk memberikan nilai yang dinama bisa merupakan nilai yang di inputkan manual */
         
-        	System.out.println("\nCetak Segitiga Sama Kaki");
+        	System.out.println("\nCetak Segitiga Sama Kaki"); //** merupakan sebuah nilai yang ditamplikan */
         	System.out.println("==========================");
         	for (int i=1;i<=x;i++)
 			{
-				//Membuat spasi
+										//Membuat spasi
 				for(int j=x-1;j>=i;j--)
 				{ 
 					System.out.print(" ");
 				}
-				//Membuat bintang
-				for(int k=1;k<=i;k++)
+										//Membuat bintang dari sistem keluaran
+				for(int k=1;k<=i;k++)				// merupakan nilai inti  yang dalam perhitungan menjadikan program sesuai yang di maksud
 				{ 
 					System.out.print("*");
 				}
@@ -43,19 +43,19 @@ public class segitiga_samakaki {
 				System.out.println();
 			}
         	
-        	System.out.print("\nIngin Mencoba lagi (Y/N) ?:");
+        	System.out.print("\nIngin Mencoba lagi (Y/N) ?:"); // sebuah kepastian bila program ini mau di coba kembali
         	String cek = scan.next();
         	
-        	if(cek.equalsIgnoreCase("N"))
+        	if(cek.equalsIgnoreCase("N"))			
         	{
         		menu = false;
         	}
-        	else if(cek.equalsIgnoreCase("Y"))
+        	else if(cek.equalsIgnoreCase("Y"))    //suatu nilai yang dari if jika ya  atau mau melanjutkan
         	{
         		menu = true;
         	}
         	else
-        	   System.exit(0);
+        	   System.exit(0); //suatu nilai keluaran jika selesai
         }   
     }
     
